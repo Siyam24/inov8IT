60d8453a4e529f366bc38408b3156a68bfdd1d68
Shamini-29 <136223488+Shamini-29@users.noreply.github.com>
2024-03-29 15:39:23 +0530

    New commit

--- back-end/src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'lnglvlatb6faflvw',
-    password: 'qglb8lww01godyk9',
-    database: 'r9a7f0kkwaquhndi',
-    port: 3306
-};
